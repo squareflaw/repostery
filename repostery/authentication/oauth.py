46d799b8a37e1d541e090a586837a65bea649f91
Albin <squareflaw@gmail.com>
2020-05-27 18:47:40 -0400

    add tests to oauth module

--- repostery/authentication/oauth.py
+++ repostery/authentication/oauth.py
@@ -8,44 +8,48 @@ class OauthTokenConverter:
     we make it a class to be able to mock it in tests easier
     """
 
-    def get_user_social_info(self, provider, access_token, code=None):
+    def get_user_social_info(self, provider, access_token=None, code=None):
         if provider == 'github':
-            return self._get_github_user_info(access_token=access_token, code=code)
-        return self._get_google_user_info(access_token)
+            return self.get_github_user_info(access_token=access_token, code=code)
+        return self.get_google_user_info(access_token)
 
-    def _get_google_user_info(self, access_token):
+    def get_google_user_info(self, access_token):
         url = f'https://www.googleapis.com/oauth2/v3/userinfo?access_token={access_token}'
-        response = requests.get(url)
-
-        if response.status_code >= 400:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
             raise AuthenticationFailed('google access token is invalid or has expired')
 
         data = response.json()
         user_info = {
-            'email': data['email'],
-            'username': data['email'],
+            'email': data.get('email'),
+            'username': data.get('email'),
             'image': data.get('picture'),
         }
         return user_info
 
-    def _get_github_user_info(self, *args, access_token=None, code=None):
+    def get_github_user_info(self, access_token=None, code=None):
         if not access_token:
             access_token = self._get_github_access_token(code)
 
         url = 'https://api.github.com/user'
-        headers = {
-            'Authorization': f'bearer {access_token}'
-        }
+        url_for_email = 'https://api.github.com/user/emails'
+        headers = {'Authorization': f'bearer {access_token}'}
 
-        response = requests.get(url, headers=headers)
-        if response.status_code >= 400:
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            email_response = requests.get(url_for_email, headers=headers)
+            email_response.raise_for_status()
+        except requests.exceptions.HTTPError:
             raise AuthenticationFailed('github access token is invalid or has expired')
 
-        email = self._get_email_from_github(response)
         data = response.json()
+        email = email_response.json()[0].get('email')
         user_info = {
             'email': email,
-            'username': data['login'],
+            'username': data.get('login'),
             'image': data.get('avatar_url'),
         }
         return user_info
@@ -58,24 +62,20 @@ class OauthTokenConverter:
         url += f'?client_id={GITHUB_CLIENT_ID}'
         url += f'&client_secret={GITHUB_CLIENT_SECRET}'
         url += f'&code={code}'
-        response = requests.post(url, headers={"Accept": "application/json"})
 
-        if response.status_code >= 400:
+        try:
+            response = requests.get(url, headers={"Accept": "application/json"})
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
             raise AuthenticationFailed('github code is invalid or has expired')
-
         response = response.json()
         return response.get('access_token')
 
-    def _get_email_from_github(self, response):
-        # fake email but can be use to query user when authenticating
-        fallback_email = response.json()['login'] + '@github.com'
-        if not response.json().get('email'):
-            email = fallback_email
-        else:
-            email = response.json().get('email')
-        return email
 
-# if __name__ == '__main__':
-#     oauth = OauthTokenConverter()
-#     info = oauth.get_user_social_info(provider='google', access_token='invalid')
-#     print(info)
+if __name__ == '__main__':
+    oauth = OauthTokenConverter()
+    info = oauth.get_user_social_info(
+        provider='github',
+        code='62d3c33477aebafe7dfa'
+    )
+    print(info)
---
+++ repostery/authentication/tests/test_oauth.py
@@ -0,0 +1,189 @@
+import requests
+import pytest
+from rest_framework.exceptions import AuthenticationFailed
+from ..oauth import OauthTokenConverter
+
+class MockResponse:
+    """
+    Mock object to reproduce the methods used in the token converter
+    """
+
+    def __init__(self, data, error=False):
+        self.data = data
+        self.raise_error = error
+
+    def json(self):
+        return self.data
+
+    def raise_for_status(self):
+        if self.raise_error:
+            raise requests.exceptions.HTTPError
+
+class TestGoogleTokenConverter:
+    """
+    Test cases for getting user info with google access token
+    """
+
+    def test_should_get_info_with_valid_access_token(self, mocker):
+        fake_google_response_data = {
+            "sub": "106623665108941395637",
+            "name": "Albin Garcia",
+            "given_name": "Albin",
+            "family_name": "Garcia",
+            "picture": "https://lh3.googleusercontent.com/a-/AOh14GhKFe1wz8TsXyJ50Zhr9GOoSd_GBSx4hUeGfnuHuw",
+            "email": "squareflaw@gmail.com",
+            "email_verified": True,
+            "locale": "es"
+        }
+        mock_response = MockResponse(fake_google_response_data)
+        mocker.patch('requests.get', return_value=mock_response)
+        token_converter = OauthTokenConverter()
+        results = token_converter.get_user_social_info(
+            provider='google',
+            access_token='valid_access_token'
+        )
+        assert results['email'] == fake_google_response_data['email']
+        assert results['image'] == fake_google_response_data['picture']
+
+    def test_should_raise_authentication_error_invalid_token(self, mocker):
+        mock_response = MockResponse(None, error=True)  # raises HTTPError
+        mocker.patch('requests.get', return_value=mock_response)
+        token_converter = OauthTokenConverter()
+        with pytest.raises(AuthenticationFailed):
+            token_converter.get_user_social_info(
+                provider='google',
+                access_token='expired_token'
+            )
+
+class TestGithubTokenConverter:
+    """
+    Test cases for getting user info with github code and access token
+    """
+
+    def test_should_get_info_with_valid_code(self, mocker):
+        fake_github_response_data = {
+            "login": "squareflaw",
+            "id": 39705088,
+            "node_id": "MDQ6VXNlcjM5NzA1MDg4",
+            "avatar_url": "https://avatars0.githubusercontent.com/u/39705088?v=4",
+            "gravatar_id": "", "url": "https://api.github.com/users/squareflaw",
+            "html_url": "https://github.com/squareflaw",
+            "followers_url": "https://api.github.com/users/squareflaw/followers",
+            "following_url": "https://api.github.com/users/squareflaw/following{/other_user}",
+            "gists_url": "https://api.github.com/users/squareflaw/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/squareflaw/starred{/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/squareflaw/subscriptions",
+            "organizations_url": "https://api.github.com/users/squareflaw/orgs",
+            "repos_url": "https://api.github.com/users/squareflaw/repos",
+            "events_url": "https://api.github.com/users/squareflaw/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/squareflaw/received_events",
+            "type": "User",
+            "site_admin": False,
+            "name": "Albin Garcia",
+            "company": None,
+            "blog": "",
+            "location": None,
+            "email": None,
+            "hireable": None,
+            "bio": "Electronics Engineer and web developer using react js and django",
+            "public_repos": 5,
+            "public_gists": 0,
+            "followers": 0,
+            "following": 0,
+            "created_at": "2018-05-28T15:36:21Z",
+            "updated_at": "2020-05-27T13:58:02Z"
+        }
+        fake_email_response_data = [
+            {
+                "email": "squareflaw@gmail.com",
+                "primary": True,
+                "verified": True,
+                "visibility": "private"
+            }
+        ]
+        mocker.patch('requests.get', side_effect=[
+            # 1st: request valid access_token using code param
+            MockResponse({"access_token": 'Valid_access_token'}),
+            MockResponse(fake_github_response_data),  # 2nd: request user info using the access_token
+            MockResponse(fake_email_response_data)  # 3rd: request user private email address
+        ])
+        token_converter = OauthTokenConverter()
+        results = token_converter.get_user_social_info(
+            provider='github',
+            code='valid_code'
+        )
+        assert results['email'] == fake_email_response_data[0]['email']
+        assert results['image'] == fake_github_response_data['avatar_url']
+
+    def test_should_get_info_with_valid_access_token(self, mocker):
+        fake_github_response_data = {
+            "login": "squareflaw",
+            "id": 39705088,
+            "node_id": "MDQ6VXNlcjM5NzA1MDg4",
+            "avatar_url": "https://avatars0.githubusercontent.com/u/39705088?v=4",
+            "gravatar_id": "", "url": "https://api.github.com/users/squareflaw",
+            "html_url": "https://github.com/squareflaw",
+            "followers_url": "https://api.github.com/users/squareflaw/followers",
+            "following_url": "https://api.github.com/users/squareflaw/following{/other_user}",
+            "gists_url": "https://api.github.com/users/squareflaw/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/squareflaw/starred{/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/squareflaw/subscriptions",
+            "organizations_url": "https://api.github.com/users/squareflaw/orgs",
+            "repos_url": "https://api.github.com/users/squareflaw/repos",
+            "events_url": "https://api.github.com/users/squareflaw/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/squareflaw/received_events",
+            "type": "User",
+            "site_admin": False,
+            "name": "Albin Garcia",
+            "company": None,
+            "blog": "",
+            "location": None,
+            "email": None,
+            "hireable": None,
+            "bio": "Electronics Engineer and web developer using react js and django",
+            "public_repos": 5,
+            "public_gists": 0,
+            "followers": 0,
+            "following": 0,
+            "created_at": "2018-05-28T15:36:21Z",
+            "updated_at": "2020-05-27T13:58:02Z"
+        }
+        fake_email_response_data = [
+            {
+                "email": "squareflaw@gmail.com",
+                "primary": True,
+                "verified": True,
+                "visibility": "private"
+            }
+        ]
+        mocker.patch('requests.get', side_effect=[
+            MockResponse(fake_github_response_data),  # 1st: request user info using the access_token
+            MockResponse(fake_email_response_data)  # 2nd: request user private email address
+        ])
+        token_converter = OauthTokenConverter()
+        results = token_converter.get_user_social_info(
+            provider='github',
+            access_token='valid_access_token'
+        )
+        assert results['email'] == fake_email_response_data[0]['email']
+        assert results['image'] == fake_github_response_data['avatar_url']
+
+    def test_should_raise_authentication_error_invalid_code(self, mocker):
+        mock_response = MockResponse(None, error=True)  # raises HTTPError
+        mocker.patch('requests.get', return_value=mock_response)
+        token_converter = OauthTokenConverter()
+        with pytest.raises(AuthenticationFailed):
+            token_converter.get_user_social_info(
+                provider='github',
+                code='expired_code'
+            )
+
+    def test_should_raise_authentication_error_invalid_access_token(self, mocker):
+        mock_response = MockResponse(None, error=True)  # raises HTTPError
+        mocker.patch('requests.get', return_value=mock_response)
+        token_converter = OauthTokenConverter()
+        with pytest.raises(AuthenticationFailed):
+            token_converter.get_user_social_info(
+                provider='github',
+                access_token='expired_access_token'
+            )
