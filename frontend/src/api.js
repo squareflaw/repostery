4cc3c4f09553ac4fecde3876c0d389524ad49158
Albin <squareflaw@gmail.com>
2020-05-30 19:28:32 -0400

    add requests methods and headers to api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,9 +3,34 @@ import {isLocalhost} from './serviceWorker'
 const API_ROOT = isLocalhost ? 'http://localhost:8000/api/v1' : 'https://repostery.herokuapp.com/api/v1'
 let token = null;
 
+const options = (method='GET', data={}) => ({
+  method: method,
+  mode: 'cors',
+  cache: 'no-cache',
+  credentials: 'same-origin',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': token? `Token ${token}`: ''
+  },
+  referrerPolicy: 'no-referrer', 
+  body: method !== 'GET'? JSON.stringify(data) : null
+})
+
 const requests = {
   get: async (url) => {
-    const response = await fetch(`${API_ROOT}${url}`);
+    const response = await fetch(`${API_ROOT}${url}`, options());
+    return await handleResponse(response);
+  },
+  post: async (url, data) => {
+    const response = await fetch(`${API_ROOT}${url}`, options('POST', data))
+    return await handleResponse(response);
+  },
+  put: async (url, data) => {
+    const response = await fetch(`${API_ROOT}${url}`, options('PUT', data))
+    return await handleResponse(response);
+  },
+  delete: async (url) => {
+    const response = await fetch(`${API_ROOT}${url}`, options('DELETE'))
     return await handleResponse(response);
   }
 }
@@ -42,7 +67,19 @@ const auth = {
   }
 }
 
+const user = {
+  getUser: async () => {
+    const url = `/user/usename`
+    return await requests.get(url)
+  },
+  getProfile: async (username) => {
+    const url = `/profiles/${username}`
+    return await requests.get(url)
+  }
+}
+
 export default {
   auth,
+  user,
   setToken: _token => {token = _token}
 }
